1f5ab380373bd0e945755dc7c04a2e429f0b0706
tigawanna <denniskinuthiaw@gmail.com>
2021-12-24 21:03:26 +0300

    added new user subscription and made it work

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,6 +1,7 @@
 const { PubSub } = require("graphql-subscriptions");
 
 const pubsub = new PubSub();
+const NEW_USER = "NEW_USER";
 
   let currentNumber = 0;
   function incrementNumber() {
@@ -17,10 +18,34 @@ const pubsub = new PubSub();
         return currentNumber;
       },
     },
+    Mutation: {
+        login: async (parent, { userInfo: { username } }, context) => {
+          // check the password
+          // await checkPassword(password);
+          return username;
+        },
+        register: (_, { userInfo: { username } }) => {
+          const user = {
+            id: 1,
+            username
+          };
+    
+          pubsub.publish(NEW_USER, {
+            newUser: user
+          });
+    
+          return {
+             user
+          };
+        }
+      },
     Subscription: {
       numberIncremented: {
         subscribe: () => pubsub.asyncIterator(["NUMBER_INCREMENTED"]),
       },
+      newUser: {
+        subscribe: () => pubsub.asyncIterator(NEW_USER)
+      }
     },
   };
   
--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -1,13 +1,31 @@
 const { gql } = require("apollo-server-express");
 
-  // Schema definition
-  const typeDefs = gql`
-    type Query {
-      currentNumber: Int
-    }
-    type Subscription {
-      numberIncremented: Int
-    }
-  `;
+// Schema definition
+const typeDefs = gql`
+  input UserInfo {
+    username: String!
+    age: Int
+  }
+  type RegisterResponse {
+    user: User
+  }
 
-  module.exports=typeDefs;
+  type User {
+    id: ID!
+    username: String
+  }
+  type Query {
+    currentNumber: Int
+  }
+
+  type Mutation {
+    register(userInfo: UserInfo!): RegisterResponse!
+    login(userInfo: UserInfo!): String!
+  }
+  type Subscription {
+    numberIncremented: Int
+    newUser: User!
+  }
+`;
+
+module.exports = typeDefs;
